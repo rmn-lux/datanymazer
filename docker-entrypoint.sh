66854fd2032864941d0346a0a03ff46f36fab527
rmn-lux <admin@it-lux.ru>
2022-05-05 17:22:17 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,25 @@
 
 set -eo pipefail
 
+check_rails_migrations() {
+    local query="select now()"
+    export PGPASSWORD="${POSTGRESQL_PASSWORD}" # env for psql pass
+
+    if [[ ! -z "${RAILS_LAST_SCHEMA_MIGRATION}" ]]; then
+        current_migration=$(psql -h "${POSTGRESQL_ADDRESS}" -p "${POSTGRESQL_PORT}" -d "${POSTGRESQL_DATABASE}" -U "${POSTGRESQL_USERNAME}" -t -c "$query" | tr -d [:space:])
+        echo "$current_migration"
+
+        # if [[ "$current_migration" != "${RAILS_LAST_SCHEMA_MIGRATION}" ]]; then
+        #     echo "Current migration in env variable $current_migration not equal migration in database"
+        #     exit 1
+        # else
+        #     echo "The migration specified in the environment variable is the same as the migration in the database, continue..."
+        # fi
+    fi
+}
+
 check_empty_vars() {
-    vars=$(env | awk -F "=" '{print $1}' | grep -v "^_\$")
+    vars=$(env | awk -F "=" '{print $1}' | egrep -v "^_\$|RAILS_LAST_SCHEMA_MIGRATION")
 
     for line in $vars
         do
@@ -14,13 +31,10 @@ check_empty_vars() {
         done
 }
 
-check_psql_alive() {
+check_psql_alive() { 
     # parse incoming string like postgres://"${POSTGRESQL_USERNAME}":"${POSTGRESQL_PASSWORD}"@"${DATABASE_URL}"/"${POSTGRESQL_DATABASE} for local check psql alive" 
-    export DATABASE_PORT="$(echo $DATABASE_URL | awk -F ":" '{print $4}' | awk -F "/" '{print $1}')"
-    export DATABASE_HOST="$(echo $DATABASE_URL | awk -F "@" '{print $2}' | awk -F ":" '{print $1}')"
-    export DATABASE_NAME=$(echo $DATABASE_URL | awk -F "@" '{print $2}' | awk -F "/" '{print $2}')
-    
-    export psql_check="pg_isready -h "${DATABASE_HOST}" -p "${DATABASE_PORT}""  
+    export POSTGRESQL_PORT="$(echo $DATABASE_URL | awk -F ":" '{print $4}' | awk -F "/" '{print $1}')"
+    export psql_check="pg_isready -h "${POSTGRESQL_ADDRESS}" -p "${POSTGRESQL_PORT}""  
 
     ${psql_check} >/dev/null || exit_code="$?" # check exit code because using set -e in script
 
@@ -51,7 +65,8 @@ pg_datanymizer --version
 check_empty_vars
 check_psql_alive
 check_s3_bucket_connection
+check_rails_migrations
 
 exec pg_datanymizer "${DATABASE_URL}" \
--c "${CONFIG_PATH}" | gzip | aws --endpoint-url "${AWS_ENDPOINT}" s3 cp - s3://"${S3_BUCKET_NAME}"/"${S3_OBFUSCATION_PATH}"/"${DATABASE_NAME}"/"$(date +%Y)"/"$(date +%m)"/$(date +%d).dump.gz \
-&& echo -e "\nDump $(date +%d).dump.gz of ${DATABASE_NAME} has been uploaded to S3 ${S3_BUCKET_NAME} bucket"
+-c "${CONFIG_PATH}" | gzip | aws --endpoint-url "${AWS_ENDPOINT}" s3 cp - s3://"${S3_BUCKET_NAME}"/"${S3_OBFUSCATION_PATH}"/"${POSTGRESQL_DATABASE}"/"$(date +%Y)"/"$(date +%m)"/$(date +%d).dump.gz \
+&& echo -e "\nDump $(date +%d).dump.gz of ${POSTGRESQL_DATABASE} has been uploaded to S3 ${S3_BUCKET_NAME} bucket"
